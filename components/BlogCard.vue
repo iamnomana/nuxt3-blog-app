9c5ac13a47a955f1a15da08939cd744f35fc0563
Divine Azianka <aziankad@gmail.com>
2022-12-13 23:47:17 +0000

    Update on BlogCard

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -10,14 +10,9 @@
       />
 
       <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 class="mb-5 text-lg tracking-tight text-gray-900 dark:text-white">
           Noteworthy technology acquisitions 2021
         </h5>
-
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-          chronological order.
-        </p>
 
         <NuxtLink
           to="/blogs/id"
