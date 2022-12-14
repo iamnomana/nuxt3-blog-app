41ce0b87a496e05cba78887b73c8e6efe5c1124f
Divine Azianka <aziankad@gmail.com>
2022-12-14 06:58:24 +0000

    Firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     head: {
       title: "Loading",
@@ -10,5 +11,33 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "@nuxtjs/firebase",
+      {
+        config: {
+          apiKey: "AIzaSyCxAADjbPDuylxfrZgMsBtJBX2R1Zx_ThE",
+          authDomain: "nuxt3-blog-app.firebaseapp.com",
+          projectId: "nuxt3-blog-app",
+          storageBucket: "nuxt3-blog-app.appspot.com",
+          messagingSenderId: "149666269247",
+          appId: "1:149666269247:web:7c98427859d06807e3508a",
+          measurementId: "G-686S00TNZX",
+        },
+        services: {
+          auth: {
+            persistence: "local", // default
+            initialize: {
+              onAuthStateChangedAction: "onAuthStateChangedAction",
+              subscribeManually: false,
+            },
+            ssr: false, // default
+          },
+          firestore: true,
+          storage: true,
+        },
+      },
+    ],
+  ],
 });
