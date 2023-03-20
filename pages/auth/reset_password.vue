723177bee1e1480be668984d32171e759b24c159
Divine Azianka <aziankad@gmail.com>
2023-03-20 23:06:18 +0000

    Update on Reset Password page

--- pages/auth/reset_password.vue
+++ pages/auth/reset_password.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <h1>Reset Password</h1>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style  scoped>
-
-</style>
+<style scoped></style>
